19c3761c55ecc6091007803c4c051edd22cc9adc
robfil50219 <robfil50219@stud.noroff.no>
2023-11-03 17:16:25 +0100

    product page harcoded html removed

--- js/product_page.js
+++ js/product_page.js
@@ -1,15 +1,36 @@
-const getproductData = async () => {
-    const urlParams = new URLSearchParams(window.location.search)
-    const productId = url.get('id'); 
-    const productDetailURL = `https://api.noroff.dev/api/v1/rainy-days/${productId}`; 
-
-    try {
-        const response = await fetch(productDetailURL)
-        if (!response.ok) {
-            throw new Error ('Network response was not ok'); 
+  const getProductData = async () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const productId = urlParams.get('id');
+        const productDetailURL = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
+      
+        try {
+          const response = await fetch(productDetailURL);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const productData = await response.json();
+      
+          displayProductDetails(productData); // Call the function to display the data
+        } catch (error) {
+          console.error('Error fetching product data', error);
         }
-        const productData = await response.json(); 
+      };
+      
+      const displayProductDetails = (product) => {
+        const productTitle = document.querySelector('h1');
+        const productImage = document.querySelector('.product-image img');
+        const productPrice = document.querySelector('.item-price');
+        
+        console.log(productImage);
 
-        displayProductDetails(productData); // Call the function to display the data 
-    }
-}; 
+        productTitle.textContent = product.title;
+        productImage.src = product.image;
+        productPrice.textContent = `$${product.price}`;
+      };
+      
+      getProductData(); // Call the getProductData function when the page loads
+      
+    
+ 
+  
+
