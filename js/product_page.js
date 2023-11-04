29c7469b1e71ddac9e3d7399c1e78e1e5cf7eb9e
robfil50219 <robfil50219@stud.noroff.no>
2023-11-04 12:30:01 +0100

    item discription added

--- js/product_page.js
+++ js/product_page.js
@@ -1,35 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
   const getProductData = async () => {
-        const urlParams = new URLSearchParams(window.location.search);
-        const productId = urlParams.get('id');
-        const productDetailURL = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
-      
-        try {
-          const response = await fetch(productDetailURL);
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const productData = await response.json();
-      
-          displayProductDetails(productData); // Call the function to display the data
-        } catch (error) {
-          console.error('Error fetching product data', error);
-        }
-      };
-      
-      const displayProductDetails = (product) => {
-        const productTitle = document.querySelector('h1');
-        const productImage = document.querySelector('.product-image img');
-        const productPrice = document.querySelector('.item-price');
-        
-        console.log(productImage);
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('id');
+    const productDetailURL = `https://api.noroff.dev/api/v1/rainy-days/${productId}`;
 
-        productTitle.textContent = product.title;
-        productImage.src = product.image;
-        productPrice.textContent = `$${product.price}`;
-      };
-      
-      getProductData(); // Call the getProductData function when the page loads
-      
+    try {
+        const response = await fetch(productDetailURL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const productData = await response.json();
+
+        displayProductDetails(productData); // Call the function to display the data
+    } catch (error) {
+        console.error('Error fetching product data', error);
+    }
+  };
+
+  const displayProductDetails = (product) => {
+    const productTitle = document.getElementById('product-title');
+    const productImage = document.getElementById('product-img');
+    const productPrice = document.querySelector('.item-price');
+    const productDescription = document.getElementById('item-description');
+
+    productTitle.textContent = product.title;
+    productImage.src = product.image;
+    productPrice.textContent = product.onSale ? `$${product.discountedPrice}` : `$${product.price}`;
+    productDescription.textContent = product.description;
+  };
+
+  getProductData(); // Call the getProductData function when the page loads
+});
+
     
  
   
