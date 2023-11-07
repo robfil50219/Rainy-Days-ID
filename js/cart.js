feffc7b0eab0ebc4dad8999b5228658d00cc4b62
robfil50219 <robfil50219@stud.noroff.no>
2023-11-07 14:05:14 +0100

    cart js

---
+++ js/cart.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded'),() => {
+    const quantityInputs = document.querySelectorAll('.quantity-input'); 
+    const totalPriceElement = document.quareySelector('.total-price'); 
+    const itemPrices = document.quareySelectorAll('.item-price'); 
+
+    //function to update price based on quantity
+
+    const updateTotalPrice = () => {
+        let totalPrice =
+    }
+}
