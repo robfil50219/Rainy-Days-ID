e5b16b5a006a0b611e4bff7ea9421d246ce62d30
robfil50219 <robfil50219@stud.noroff.no>
2023-12-05 10:37:09 +0100

    cart js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
+// index.js
+
+import { addItemToCart, getCartItems } from './cart';
+
 document.addEventListener('DOMContentLoaded', () => {
   const apiURL = 'https://api.noroff.dev/api/v1/rainy-days';
   const loadingIndicator = document.getElementById('loading-indicator');
+  const productContainer = document.querySelector('.body-items .row');
+  const cartSummaryContainer = document.querySelector('.cart-summary');
 
   // Fetch data from the API
   const fetchData = async () => {
@@ -15,14 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
     } catch (error) {
       console.error('Error fetching data', error);
     } finally {
-      loadingIndicator.style.display = 'none' // Hide the loading indicator when the data is loaded
+      loadingIndicator.style.display = 'none'; // Hide the loading indicator when the data is loaded
     }
   };
 
   // Function to display products
-  const displayProducts = (products) => { 
-    const productContainer = document.querySelector('.body-items .row');
-
+  const displayProducts = (products) => {
     products.forEach((product) => {
       const item = document.createElement('div');
       item.classList.add('item');
@@ -33,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const buyButton = document.createElement('button');
       buyButton.classList.add('buy-button');
       buyButton.textContent = 'Buy Now';
+      buyButton.addEventListener('click', () => addItemToCart(product.id)); // Add item to cart on button click
       buyNowDiv.appendChild(buyButton);
 
       const productLink = document.createElement('a');
@@ -64,7 +69,44 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   };
 
+  // Function to render cart summary
+  const renderCartSummary = () => {
+    cartSummaryContainer.innerHTML = '';
+    const cartItems = getCartItems();
+
+    let totalItems = 0;
+    let totalPrice = 0;
+
+    cartItems.forEach((cartItem) => {
+      const product = products.find((p) => p.id === cartItem.id);
+
+      if (product) {
+        totalItems += cartItem.quantity;
+        totalPrice += product.price * cartItem.quantity;
+      }
+    });
+
+    const totalItemsParagraph = document.createElement('p');
+    totalItemsParagraph.textContent = `Total Items: ${totalItems}`;
+
+    const totalPriceParagraph = document.createElement('p');
+    totalPriceParagraph.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+
+    const checkoutButton = document.createElement('a');
+    checkoutButton.href = 'checkout.html';
+    checkoutButton.classList.add('checkout-button');
+    checkoutButton.textContent = 'Checkout';
+
+    cartSummaryContainer.appendChild(totalItemsParagraph);
+    cartSummaryContainer.appendChild(totalPriceParagraph);
+    cartSummaryContainer.appendChild(checkoutButton);
+  };
+
   // Call the fetchData function when the page loads
   fetchData();
+
+  // Render cart summary on page load
+  renderCartSummary();
 });
+
 
